a63e04562d4cdf47351f23c3f24c43717262b10d
PavelPotapov <pavelpotapovv96@gmail.com>
2023-11-29 06:08:56 +0500

    сделал вторую страничку studia

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+import { resolve } from "path"
 import { defineConfig } from "vite"
+
+const root = resolve(__dirname, "src")
+const ourDit = resolve(__dirname, "build")
 
 export default defineConfig({
 	root: "./src",
 	build: {
 		outDir: "../build",
 		assetsDir: "assets",
+		input: {
+			main: resolve(__dirname, "index.html"),
+			about: resolve(__dirname, "pages", "about", "index.html")
+		}
 	},
 })
